55eff9329a6409838ff1de4ac7be2aabef6fe125
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2015-01-15 20:43:46 -0800

    display the original score in a gray, small text.

--- myscript.js
+++ myscript.js
@@ -17,19 +17,18 @@ function patch(){
       honest = 1.0;
     }
 
-    console.log("Original score: " + score + ", Honest score: " + honest);
+    console.log("Original score: " + score.toFixed(2) + ", Honest score: " + honest);
     return honest;
   };
 
-  $('[rel="v:rating"], .rate .score').each(function(index, element) {
-    var el = $(element);
-    var score = parseFloat(el.text());
+  $('.rate .score').each(function(index, element) {
+    var score = parseFloat($(element).text());
     if (isNaN(score)) return;
 
     var honest = honestify(score);
-    var image = "http://image1-3.tabelog.k-img.com/images/restaurant/star/star_ll" + (honest * 10) + ".gif";
-    el.text(honest.toFixed(1));
-    el.parent().find("img").attr("src", image);
+    $(element).html(honest.toFixed(1) + ' <span class="tabelog-score">(' + score.toFixed(2) + ")</span>");
+    $(element).parent().find("img")
+     .attr("src", "http://image1-3.tabelog.k-img.com/images/restaurant/star/star_ll" + (honest * 10) + ".gif");
   });
 
   $(".info .score-overall").each(function(index, element) {
@@ -37,10 +36,14 @@ function patch(){
     if (isNaN(score)) return;
 
     var honest = honestify(score);
-    $(element).find(".score").text(honest.toFixed(1));
+    $(element).find(".score").html(honest.toFixed(1) + ' <span class="tabelog-score">(' + score.toFixed(2) + ")</span>");
     $(element).find(".star").removeClass().addClass("star star" + honest * 10);
   });
 }
+
+var style = document.createElement('style');
+style.appendChild(document.createTextNode('.tabelog-score { font-weight: normal; font-size: 60%; color: #bbb }'));
+document.body.appendChild(style);
 
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+patch+')();'));
